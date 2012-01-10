4f1d0a5bb786ba2b6559506f6cbc0959b0e96a12
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-01-10 22:09:00 +0200

    Add ACS class to Mono.Terminal.
    Contains Special Characters!

---
+++ Mono.Terminal/ACS.cs
@@ -0,0 +1,19 @@
+namespace Mono.Terminal
+{
+	public static class ACS
+	{
+		public const int LLCORNER = unchecked((int)0x40006d);
+		public const int LRCORNER = unchecked((int)0x40006a);
+		public const int HLINE    = unchecked((int)0x400071);
+		public const int ULCORNER = unchecked((int)0x40006c);
+		public const int URCORNER = unchecked((int)0x40006b);
+		public const int VLINE    = unchecked((int)0x400078);
+		public const int LTEE     = unchecked((int)0x400074);
+		public const int RTEE     = unchecked((int)0x400075);
+		public const int BTEE     = unchecked((int)0x400076);
+		public const int TTEE     = unchecked((int)0x400077);
+		public const int PLUS     = unchecked((int)0x40006e);
+		public const int DIAMOND  = unchecked((int)0x400060);
+	}
+}
+
